5d2c9e51a7874cad2c562c03c168c577468c0a11
jasonhung11 <jasonhung9923@gmail.com>
2022-12-29 14:45:23 +0800

    update path config

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,8 +1,8 @@
-import AboutMe from "$components/AboutMe";
-import Certificate from "$components/Certificate";
-import Projects from "$components/Projects";
-import Skills from "$components/Skills";
-import Works from "$components/Works";
+import AboutMe from "@components/AboutMe";
+import Certificate from "@components/Certificate";
+import Projects from "@components/Projects";
+import Skills from "@components/Skills";
+import Works from "@components/Works";
 
 export default function Home(){
     return (
