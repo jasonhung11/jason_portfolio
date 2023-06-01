4deb18ff5709b94f61c1de85b47e9ae19c798634
jasonhung-cstl <jason.hung@cstl.com.hk>
2023-06-01 17:37:00 +0800

    add certificate

--- src/components/Certificate/index.tsx
+++ src/components/Certificate/index.tsx
@@ -1,7 +1,16 @@
-export default function Certificate(){
-    return (
-        <>
-            
-        </>
-    )
+import { Box, Card } from "@mui/material";
+import saa_03 from "../../images/aws-certified-solutions-architect-associate.png";
+export default function Certificate() {
+  return (
+    <div style={{ padding: "0% 20% 2% 20%" }}>
+      <Card style={{ boxShadow: "0.5rem" }}>
+        <a
+          href="https://www.credly.com/badges/25c16f1e-bc70-461c-9118-0af2c63e907a/public_url"
+          target="_blank"
+        >
+          <img src={saa_03}></img>
+        </a>
+      </Card>
+    </div>
+  );
 }
