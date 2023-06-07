c52b7c68bcbee4dd453d7af11893668dc24b06dd
jasonhung11 <jasonhung9923@gmail.com>
2023-06-07 17:59:42 +0800

    update style

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -6,12 +6,23 @@ import React from "react";
 
 export default function AboutMe() {
   const displayIcon = useMediaQuery("(min-width: 700px)");
+  const isMatch_1200 = useMediaQuery("(min-width: 1200px)");
   // fix Icon and Skills spacing
   const aboutMeStyle = {
-    display: displayIcon ? "flex" : "block",
+    display: isMatch_1200 ? "flex" : "block",
     margin: "2% 15%",
     justifyContent: "center",
   };
+
+  const skillsGridStyle = {
+    width: isMatch_1200 ? "70%" : "100%",
+    padding: "2% 0" 
+  }
+
+  const iconStyle = {
+    width: '400px',
+    height: 'auto'
+  }
   return (
     <Grid style={aboutMeStyle}>
       {/* {displayIcon && (
@@ -20,14 +31,14 @@ export default function AboutMe() {
         </Grid>
       )} */}
       <Grid>
-        <MyImage src={me} />
+        <MyImage src={me} style={iconStyle}/>
       </Grid>
 
       <Grid style={{ alignItems: "center", display: "flex" }}>
         <Grid>
           <h1>Welcome to Jason's Portfolio</h1>
           <p style={{ alignItems: "center" }}>I'm Jason xxxxxxx</p>
-          <Card style={{ width: "70%", padding: "2% 0" }}>
+          <Card style={skillsGridStyle}>
             <Skills />
           </Card>
           <Button>Download MY CV</Button>
